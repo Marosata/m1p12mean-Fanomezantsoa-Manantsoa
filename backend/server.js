0aa10bbeab9e12ab453b2e2cb1fa719dee7a47b4
Fanomezantsoa <marosataf@gmail.com>
2025-03-14 15:00:08 +0300

    modification de package json et deploiement dans render

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,12 @@ app.use(express.json());
 app.use(cors());
 
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connecté à MongoDB Atlas"))
+  .catch((err) => console.error("Erreur de connexion à MongoDB Atlas:", err));
+  
 app.use("/students", studentRoutes);
 
 app.listen(3000, () => console.log("Serveur démarré sur le port 3000"));

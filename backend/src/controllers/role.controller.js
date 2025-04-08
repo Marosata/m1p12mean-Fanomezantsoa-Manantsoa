a59c7a000bbcbc1fd50d7884bb7ebd59df30ddf8
Fanomezantsoa <107981926+Marosata@users.noreply.github.com>
2025-04-08 10:09:00 +0300

    Update role.controller.js

--- backend/src/controllers/role.controller.js
+++ backend/src/controllers/role.controller.js
@@ -1,15 +1,12 @@
 const config = require("../config/auth.config");
 const db = require("../models");
 const Role = db.role;
-exports.findRole = (req, res) => { 
-    console.log(req.body)
-    Role.find(
-      (err, roles) => {
-        if (err) {
-          res.status(500).send({ message: err });
-          return;
-        }
-        res.send(roles);
-      }
-    )
+exports.findRole = async (req, res) => {
+  try{
+    console.log(req.body);
+    Role.find();
+  }catch(error){
+    console.error(error)
+    res.status(500).json({message: error.message});
   }
+};
